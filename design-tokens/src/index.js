fd6bcc09dc6be686e728bfe300c38bdfcd69d8f9
Luiz Henrique <luizhjramos@outlook.com>
2022-10-16 17:16:55 -0300

    feat: call getBrands utility function to build dist folder

---
+++ design-tokens/src/index.js
@@ -0,0 +1,16 @@
+const path = require("path");
+const { getBrands } = require("./brand");
+
+registerFilter();
+registerFormat();
+
+// dist/scss/globals.scss
+// dist/scss/marca-a/tema-1/dark.scss
+
+getBrands().map(async function (current) {
+  const buildPath = {
+    css: path.join("dist", "css", current.dest, path.sep),
+  };
+
+  await buildTokens({ current, buildPath });
+});
