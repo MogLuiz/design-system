adde0c195262bbe80a709094791503c43d83333b
Luiz Henrique <luizhjramos@outlook.com>
2022-10-18 05:48:31 -0300

    chore(config): register new sass mixins format

--- design-tokens/src/config.js
+++ design-tokens/src/config.js
@@ -50,7 +50,58 @@ function registerFilter() {
   });
 }
 
+function registerFormat() {
+  StyleDictionaryPackage.registerFormat({
+    name: "scss/mixin",
+    formatter: function ({ dictionary }) {
+      let output = "";
+      dictionary.allProperties.map((prop) => {
+        if (prop.attributes.category == "switch") {
+          output += `
+						@if $type == switch-${prop.attributes.type} {
+							transition-duration: ${prop.value.velocity};
+							transition-timing-function: ${prop.value.vibe};
+						}
+					`;
+        }
+        if (prop.attributes.category == "spin") {
+          output += `
+						@if $type == spin-${prop.attributes.type} {
+							transition-duration: ${prop.value.velocity};
+							transition-timing-function: ${prop.value.vibe};
+							#{$trigger} {
+								transform: rotate(${prop.value.rotation});
+							}
+						}
+					`;
+        }
+        if (prop.attributes.category == "expand") {
+          output += `
+						@if $type == expand-${prop.attributes.type} {
+							transition-duration: ${prop.value.velocity};
+							transition-timing-function: ${prop.value.vibe};
+							#{$trigger} {
+								transform: scale(${prop.value.scale});
+							}
+						}
+					`;
+        }
+      });
+
+      //  @include motion-token(spin-fast, '.actived')
+
+      return `
+@mixin motion-token($type, $trigger){
+  ${output}
+}
+
+      `;
+    },
+  });
+}
+
 module.exports = {
   registerConfig,
   registerFilter,
+  registerFormat,
 };
--- design-tokens/src/index.js
+++ design-tokens/src/index.js
@@ -1,9 +1,10 @@
 const path = require("path");
 const { getBrands } = require("./brand");
 const { buildTokens } = require("./build");
-const { registerFilter } = require('./config');
+const { registerFilter, registerFormat } = require("./config");
 
 registerFilter();
+registerFormat();
 
 // dist/scss/globals.scss
 // dist/scss/marca-a/tema-1/dark.scss
@@ -11,7 +12,7 @@ registerFilter();
 getBrands().map(async function (current) {
   const buildPath = {
     css: path.join("dist", "css", current.dest, path.sep),
-    scss: path.join('dist', 'scss', current.dest, path.sep),
+    scss: path.join("dist", "scss", current.dest, path.sep),
   };
 
   await buildTokens({ current, buildPath });
